d870f2f0883f891ad6c32ce58ab7a7bfd3102e6b
lwang114 <lwang114@illinois.edu>
2020-10-07 21:35:05 -0500

    rcnn feature prep code under vgg16

--- vgg16/read_tsv.py
+++ vgg16/read_tsv.py
@@ -8,6 +8,8 @@ import sys
 import zlib
 import time
 import mmap
+import json
+import os
 
 csv.field_size_limit(sys.maxsize)
    
@@ -16,12 +18,39 @@ infile = '/ws/ifp-53_2/hasegawa/lwang114/data/mscoco/trainval_36/trainval_resnet
 
 
 if __name__ == '__main__':
-
+  for x in ['train', 'val']:
+    feat_root = '/ws/ifp-53_2/hasegawa/lwang114/data/mscoco/{}2014/rcnn_feats/'.format(x)
+    if not os.path.isdir(feat_root):
+        os.mkdir(feat_root)
+    bbox_file = '/ws/ifp-53_2/hasegawa/lwang114/data/mscoco/{}2014/mscoco_{}_bboxes.txt'.format(x, x)
+    bbox_outfile = '/ws/ifp-53_2/hasegawa/lwang114/data/mscoco/{}2014/mscoco_{}_bboxes_rcnn.json'.format(x, x)
+    
+    # TODO Read a list of image ids
+    img_ids = [] 
+    img_id = ''
+    with open(bbox_file, 'r') as f:
+      for line in f:
+        cur_img_id = line.split()[0]
+        if cur_img_id != img_id:
+          img_ids.append(cur_img_id)
+          img_id = cur_img_id
+    
     # Verify we can read a tsv
     in_data = {}
-    with open(infile, "r+b") as tsv_in_file:
+    with open(infile, "r+b") as tsv_in_file,\
+         open(bbox_outfile, 'w') as json_out_file:
         reader = csv.DictReader(tsv_in_file, delimiter='\t', fieldnames = FIELDNAMES)
+        i_item = 0
         for item in reader:
+            for ex, img_id in enumerate(img_ids):
+                if int(img_id.split('_')[-1]) == int(item['image_id']):
+                    break
+
+            arr_key = '{}_{}'.format(img_id, ex)
+            print('Instance {} with image id key {}'.format(i_item, arr_key))
+            # if i_item > 30:
+            #     break
+            i_item += 1
             item['image_id'] = int(item['image_id'])
             item['image_h'] = int(item['image_h'])
             item['image_w'] = int(item['image_w'])   
@@ -29,6 +58,9 @@ if __name__ == '__main__':
             for field in ['boxes', 'features']:
                 item[field] = np.frombuffer(base64.decodestring(item[field]), 
                       dtype=np.float32).reshape((item['num_boxes'],-1))
-            in_data[item['image_id']] = item
-            break
-    print in_data
+
+            np.save('{}/{}.npy'.format(feat_root, arr_key), item['features'])
+            in_data[arr_key] = item['boxes'].tolist()
+            # break
+        json.dump(in_data, json_out_file, indent=4, sort_keys=True)
+    # print in_data
